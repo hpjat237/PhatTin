13fdb68028af76fb803b0e0d16f7dece4fd0e0b6
P4nzerr <enderthelord@gmail.com>
2024-11-10 15:59:22 +0700

    fix repo

--- src/main/java/vn/dodientu/repository/OrderShipperRepository.java
+++ src/main/java/vn/dodientu/repository/OrderShipperRepository.java
@@ -1,8 +1,8 @@
 package vn.dodientu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import vn.dodientu.model.Order;
 import vn.dodientu.model.OrderShipper;
+import vn.dodientu.model.OrderShipperId;
 
-public interface OrderShipperRepository extends JpaRepository<OrderShipper, Order> {
+public interface OrderShipperRepository extends JpaRepository<OrderShipper, OrderShipperId> {
 }
--- src/main/java/vn/dodientu/repository/UserRoleRepository.java
+++ src/main/java/vn/dodientu/repository/UserRoleRepository.java
@@ -2,6 +2,7 @@ package vn.dodientu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import vn.dodientu.model.UserRole;
+import vn.dodientu.model.UserRoleId;
 
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleId> {
 }
